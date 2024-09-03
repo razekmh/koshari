1d82cd2029f501cab74d8cbd8dc18930b89a9b15
Mahmoud Abdelrazek <razekmh@gmail.com>
2024-09-03 14:06:01 +0100

    init pckg

---
+++ src/koshari/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about koshari."""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Mahmoud Abdelrazek"
+__email__ = "razekmh@gmail.com"
+__version__ = "0.1.0"
